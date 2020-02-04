7bf6c072a04d65f20cb07779a74fc3eb42675b3d
belli29 <belli29@hotmail.com>
2020-02-04 05:10:09 +0000

    set up basic app structure and connection wth Atlas Mongo DB

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
 import os
-from flask import Flask
+from flask import Flask, url_for, render_template, redirect, request
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+    import env
+
 
 app = Flask(__name__)
 
+app.config['MONGO_DBNAME'] = os.environ.get('MONGODB_NAME')
+app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
+mongo = PyMongo(app)
+
 @app.route('/')
-def hello():
-    return "Hello World"
+@app.route('/get_books')
+def get_books():
+    return render_template('books.html', books=mongo.db.books.find())
 
 if __name__ == '__main__':
     app.run(host = os.environ.get('IP'),
             port = int(os.environ.get('PORT')),
-            debug = True)
+            debug = True
+            )
