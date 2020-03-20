c8c4ce44fe5b48369159e891217a7e1f5fb8c9b0
belli29 <belli29@hotmail.com>
2020-03-20 19:35:19 +0000

    rating system replaced with a visual stars rating

---
+++ static/js/rating.js
@@ -0,0 +1,6 @@
+// if one star is clicked all the previous ones (including the clicked one) get black ('&#10029;')
+
+function bookRating() {
+    $("body").css('background-color', 'blue')
+}
+console.log("its working") 
